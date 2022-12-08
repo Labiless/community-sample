1c499a901107fa0414f5150663989d546ee82dfe
unknown <alessandromiele94@hotmail.it>
2022-12-08 14:37:17 +0100

    feat: added ws server, ws client manager and some test

---
+++ ws-server.js
@@ -0,0 +1,55 @@
+const port = 8080;
+
+const { json } = require('express');
+//http server setup
+const express = require('express');
+const app = express();
+var http = require("http");
+var server = http.createServer(app);
+
+//web scoket setup
+const WebSocket = require('ws');
+const wss = new WebSocket.WebSocketServer({ 
+    port
+});
+
+//start server
+//server.listen(port);
+
+//WEBSOCKET
+const allConncetion = {};
+wss.on('connection', (ws) => {
+    console.log("New connection request...");
+    ws.on('message', (message) => onMessage(message, ws));
+});
+const onMessage = (message, ws) => {
+    performMessageAction(JSON.parse(message.toString()), ws);
+}
+const sendMessage = (ws, message) => {
+    try {
+        ws.send(JSON.stringify(message));
+    } catch (error) {
+        console.log("error send message")
+    }
+}
+const performMessageAction = (message, ws) => {
+    switch (message.code) {
+        case "openConnection":
+            allConncetion[message.body] = ws;
+            console.log("connection extablish: " + message.body)
+            sendMessage(ws, { code: "openConnection", body: "connection extablish: " + message.body });
+            break;
+        case "test":
+            console.log("test message: " + message.body);
+            sendMessage(ws, {code: "test", body: "test message"})
+        case "keepAlive":
+            sendMessage(allConncetion["head"], JSON.stringify(message));
+            break;
+        case "closeConnection":
+            delete allConncetion[message];
+            console.log("delete the connection: " + message);
+            break;
+        default:
+            break;
+    }
+}
